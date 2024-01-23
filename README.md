2ecb6750fb2a875d86462901345a6ac10c87d965
Whit Will <147825238+Whit-Williams@users.noreply.github.com>
2024-01-23 18:03:52 +0000

    Added css styles to html

--- README.md
+++ README.md
@@ -1,1 +1,36 @@
-# Team-Profile-Generator
+# Team Profile Generator
+
+Welcome to The Team Profile Generator App.
+
+Following on from our lesson on Object Oriented Programming, I have created a Team Profile generator using npm Inquirer and Node.js.
+
+## About The Project
+
+The task for this challenge was to build an application that allows a team manager to generate a html page that displays their team's employee details. The employee details are created by answering questions prompted by Inquirer using the command line. Once the file is generated it is then saved to a folder called "output".
+
+![Screenshot image of application - ADD](./assets/images/generator-screenshot.png)
+
+
+### Technologies
+This project is created with:
+
+- Visual Studio Code 1.83.1
+- Git Bash 5.2.15
+- Node.js
+- npm Inquirer
+- Bootstrap 4
+
+### Key Features
+- Displays prompts in the terminal using Node.js
+- Answers are saved to a new HTML file
+- Team member information is displayed in Boostrap card format, styled with custom CSS
+
+### How to Use This Application
+
+
+### Credits 
+-[Stack Overflow](stackoverflow.com)
+- Google Searches
+
+### License 
+None
--- src/page-template.js
+++ src/page-template.js
@@ -13,7 +13,7 @@ const generateTeam = team => {
         <div class="card employee-card">
         <div class="card-header">
             <h2 class="card-title">${manager.getName()}</h2>
-            <h3 class="card-title"><i class="fas fa-mug-hot mr-2"></i>${manager.getRole()}</h3>
+            <h3 class="card-title">${manager.getRole()} <i class="fas fa-mug-hot mr-2"></i></h3>
         </div>
         <div class="card-body">
             <ul class="list-group">
@@ -32,7 +32,7 @@ const generateTeam = team => {
         <div class="card employee-card">
     <div class="card-header">
         <h2 class="card-title">${engineer.getName()}</h2>
-        <h3 class="card-title"><i class="fas fa-glasses mr-2"></i>${engineer.getRole()}</h3>
+        <h3 class="card-title">${engineer.getRole()} <i class="fas fa-glasses mr-2"></i></h3>
     </div>
     <div class="card-body">
         <ul class="list-group">
@@ -51,7 +51,7 @@ const generateTeam = team => {
         <div class="card employee-card">
     <div class="card-header">
         <h2 class="card-title">${intern.getName()}</h2>
-        <h3 class="card-title"><i class="fas fa-user-graduate mr-2"></i>${intern.getRole()}</h3>
+        <h3 class="card-title">${intern.getRole()} <i class="fas fa-user-graduate mr-2"></i></h3>
     </div>
     <div class="card-body">
         <ul class="list-group">
@@ -97,6 +97,9 @@ module.exports = team => {
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <meta http-equiv="X-UA-Compatible" content="ie=edge" />
     <title>My Team</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Anton&family=Poppins:ital,wght@0,600;0,700;0,800;0,900;1,700&family=Tilt+Warp&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
         integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <link rel="stylesheet" href="style.css">
@@ -107,13 +110,13 @@ module.exports = team => {
     <div class="container-fluid">
         <div class="row">
             <div class="col-12 jumbotron mb-3 team-heading">
-                <h1 class="text-center">My Team</h1>
+                <h1 class="text-center">Team Dashboard</h1>
             </div>
         </div>
     </div>
     <div class="container">
         <div class="row">
-            <div class="team-area col-12 d-flex justify-content-center">
+            <div class="team-area col-12 d-flex justify-content-around">
                 ${generateTeam(team)}
             </div>
         </div>
@@ -122,3 +125,4 @@ module.exports = team => {
 </html>
     `;
 };
+
